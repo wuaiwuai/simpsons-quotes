e422f264fa23b911e08ca229bed7891e56ce3983
jgwil2 <jgwil2@gmail.com>
2014-07-30 22:26:34 +0200

    stuff

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $.getJSON( "data.json", function(data) {
 	var currentItem = Math.floor(Math.random() * length)
 
 	var template = $('#quoteTemplate').html();
-	$('#content').html(_.template(template, {data:items}));
+	$('#content').html(_.template(template, {data: items[currentItem]}));
 	$('#content').css('background-image','url(img/' + items[currentItem]["bg-img"] + ')')
 
 });
